f496288e0819f8c4eb667c9306f7b1d072b09738
Lukeuke <lukasz.mrowiec10102004@gmail.com>
2022-07-01 17:43:16 +0200

    Added GameService.cs Tests

---
+++ Application.Test/GameServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using Application.Services;
+using NUnit.Framework;
+
+namespace Application.Test;
+
+public class GameServiceTest
+{
+    private string _word;
+    
+    private readonly WordProviderService _wordProviderService = new ();
+    
+    [Test]
+    public void GetWord_Test()
+    {
+        var word = _wordProviderService.GetWordFromApi();
+        _word = word.Result;
+        
+        Assert.AreEqual(word.Result, _word);
+    }
+
+    [Test]
+    public void CheckForWord_Test()
+    {
+        var output = GameService.GetInstance().CheckForWord_Mock("wrong", "wrung");
+        var output1 = GameService.GetInstance().CheckForWord_Mock("wrong", "wrong");
+        
+        Assert.AreEqual(output, "o");
+        Assert.AreEqual(output1, string.Empty);
+        
+        Assert.AreNotEqual(output, "wrong");
+        Assert.AreNotEqual(output, string.Empty);
+    }
+}
--- Application/Services/GameService.cs
+++ Application/Services/GameService.cs
@@ -16,9 +16,23 @@ public sealed class GameService
         return _instance ??= new GameService();
     }
 
+    
     public string CheckForWord(string guessedWord)
     {
         return guessedWord.Where((t, i) => t != Word[i]).Aggregate("", (current, t) => current + t);
+    }
+    
+    /// <summary>
+    /// Mocked method of CheckForWord to test it,
+    /// not quite sure if its good way to do this
+    /// </summary>
+    /// <param name="guessedWord"></param>
+    /// <param name="word"></param>
+    /// <returns>Wrong letters in guessedWord, compared to word</returns>
+    public string CheckForWord_Mock(string guessedWord, string word)
+    {
+        Word = word;
+        return CheckForWord(guessedWord);
     }
     
     public void SomeBusinessMethod()
