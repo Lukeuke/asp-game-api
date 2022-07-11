9dba351f9f554aeea368fb57a8e715c16a5f879c
Lukeuke <lukasz.mrowiec10102004@gmail.com>
2022-07-11 21:16:19 +0200

    Modified WordProviderService & added own word API in case of 1st one wont work

---
+++ Application/DTO/Separate/WordsDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.DTO.Separate;
+
+/// <summary>
+/// This model is for getting list of words from heroku API
+/// </summary>
+public class WordsDto
+{
+    public List<string> Words { get; set; }
+}
--- Application/Services/WordProviderService.cs
+++ Application/Services/WordProviderService.cs
@@ -1,21 +1,36 @@
-﻿using Newtonsoft.Json;
+﻿using Application.DTO.Separate;
+using Newtonsoft.Json;
 
 namespace Application.Services;
 
 public class WordProviderService : IWordProviderService
 {
     private const string Url = "https://random-word-api.herokuapp.com/word?length=5";
+    private const string AltUrl = "https://eng-words-api.herokuapp.com/words?length=5&count=1"; // My Api made in node.js in case first wont work
     
     public async Task<string> GetWordFromApi()
     {
-        using var httpClient = new HttpClient();
-        var result = await httpClient.GetAsync(Url);
-        var json = await result.Content.ReadAsStringAsync();
+        try
+        {
+            using var httpClient = new HttpClient();
+            var result = await httpClient.GetAsync(Url);
+            var json = await result.Content.ReadAsStringAsync();
         
-        var word = JsonConvert.DeserializeObject<List<string>>(json);
+            var word = JsonConvert.DeserializeObject<List<string>>(json);
 
-        Console.WriteLine(word[0]);
+            return word![0];
+        }
+        catch
+        {
+            // I've put its in catch because somehow its slower than this first one
+            // ~ 1000ms vs 300-200ms
+            using var httpClient = new HttpClient();
+            var result = await httpClient.GetAsync(AltUrl);
+            var json = await result.Content.ReadAsStringAsync();
         
-        return word[0];
+            var wordsDto = JsonConvert.DeserializeObject<WordsDto>(json);
+            
+            return wordsDto!.Words[0];
+        }
     }
 }
